2e9de444088665b454cb0f03b5c26158ed92b989
Unkn0wn-Human <75699858+Unkn0wn-Human@users.noreply.github.com>
2022-05-07 23:17:49 -0400

    Commit test

--- src/main/java/net/isaacj/tfw/TFWmod.java
+++ src/main/java/net/isaacj/tfw/TFWmod.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 public class TFWmod implements ModInitializer {
 
+
+	//mod id
 	public static final String MOD_ID = "tfw";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
