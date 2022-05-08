b1bc70b3e45bbe2d395c0c2ab8294a03f5fb28b1
Unkn0wn-Human <75699858+Unkn0wn-Human@users.noreply.github.com>
2022-05-07 23:15:41 -0400

    Initial commit

---
+++ src/main/java/net/isaacj/tfw/TFWmod.java
@@ -0,0 +1,18 @@
+package net.isaacj.tfw;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TFWmod implements ModInitializer {
+
+	public static final String MOD_ID = "tfw";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
